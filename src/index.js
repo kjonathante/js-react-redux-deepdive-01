9a74a1774a2aa6494029891e516c9b02eb5ab598
Kit Jonathan Te <kjonathante@gmail.com>
2019-04-24 12:56:34 -0700

    Created App stateless component.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
-
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h2>Type in a new Message:</h2>
+      <input />
+      <br />
+      <button>Submit</button>
     </div>
   );
 }
