94000ebaa3842c12cbb3eb34e4833cdba8fa5bcc
Kit Jonathan Te <kjonathante@gmail.com>
2019-04-24 13:17:01 -0700

    Converted stateless App to an App class component.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div>
-      <h2>Type in a new Message:</h2>
-      <input />
-      <br />
-      <button>Submit</button>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    input: ""
+  };
+
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = event.target;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      input: ""
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        <input
+          name="input"
+          value={this.state.input}
+          onChange={this.handleInputChange}
+        />
+        <br />
+        <button onClick={this.handleFormSubmit}>Submit</button>
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
